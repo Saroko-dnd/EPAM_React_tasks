a8b08d6d96a62b29c9abac879d7c80def9a30715
Ihar Saroka <Ihar_Saroka@epam.com>
2018-06-15 13:22:30 +0300

    master: added all necessary config files

---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// We need UglifyJs because optimization.minimizer overrides the defaults provided by webpack 4
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: ['babel-polyfill', './src/index.js'],
+    devServer: {
+        contentBase: './dist',
+        open: true,
+        historyApiFallback: {
+            index: 'index.html',
+        },
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true,
+            }),
+            new OptimizeCSSAssetsPlugin({}),
+        ],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: { minimize: true },
+                    },
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    { loader: 'babel-loader' },
+                    {
+                        loader: 'eslint-loader',
+                        options: {
+                            fix: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: false,
+                            sourceMap: true,
+                            importLoader: 2,
+                        },
+                    },
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        }),
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html',
+        }),
+    ],
+    watch: false,
+};
