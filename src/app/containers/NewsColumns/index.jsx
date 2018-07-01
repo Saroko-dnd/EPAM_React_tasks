385c26c333e320b8ad55e7796b4668124e8b35a5
Ihar Saroka <Ihar_Saroka@epam.com>
2018-07-01 12:39:18 +0300

    ReduxIntegration: improved mapStateToProps function for NewsColumns container

--- src/app/containers/NewsColumns/index.jsx
+++ src/app/containers/NewsColumns/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import _ from 'lodash';
 import { connect } from 'react-redux';
 
 import NewsColumns from './NewsColumns';
@@ -6,30 +7,21 @@ import actions from '../../actions';
 import ArticlesList from '../../components/ArticlesList';
 
 const mapStateToProps = (state) => {
-    const groupedArticles = [];
-    let sliceStartIndex;
-    let sliceEndIndex;
-
-    if (state.articles.length) {
-        for (let column = 0; column < 4; column += 1) {
-            sliceStartIndex = 5 * column;
-            sliceEndIndex = sliceStartIndex + 5;
-
-            groupedArticles.push(<ArticlesList
-                className="col-xl-3 col-lg-4 col-md-6"
-                key={state.articles[sliceStartIndex].url}
-                articles={state.articles.slice(
-                    sliceStartIndex,
-                    sliceEndIndex,
-                )}
-            />);
-        }
-    }
-
-    return {
-        columns: groupedArticles,
+    const groupedArticles = _.chunk(state.articles, 5);
+    const props = {
+        columns: [],
         dataUploaded: !state.newsIsLoading,
     };
+
+    for (let column = 0; column < groupedArticles.length; column += 1) {
+        props.columns.push(<ArticlesList
+            className="col-xl-3 col-lg-4 col-md-6"
+            key={groupedArticles[column][0].url}
+            articles={groupedArticles[column]}
+        />);
+    }
+
+    return props;
 };
 
 const mapDispatchToProps = dispatch => ({
