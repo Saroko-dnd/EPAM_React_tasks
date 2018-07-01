00c4e62191844d1f3db60c2d0448e5cc8d36b494
Ihar Saroka <Ihar_Saroka@epam.com>
2018-07-01 13:39:58 +0300

    ReduxIntegration: added connected-react-router for dispathing history methods in future

--- src/index.jsx
+++ src/index.jsx
@@ -1,29 +1,38 @@
 import React from 'react';
+import Thunk from 'redux-thunk';
+import { createBrowserHistory } from 'history';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import { combineReducers, createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+// import { BrowserRouter } from 'react-router-dom';
+import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
+import {
+    connectRouter,
+    routerMiddleware,
+    ConnectedRouter,
+} from 'connected-react-router';
 
 import 'bootstrap/scss/bootstrap.scss';
-
 import './assets/scss/index.scss';
 
 import App from './app';
 import reducers from './app/reducers';
 import constants from './app/constants';
 
+const history = createBrowserHistory();
+
 const store = createStore(
-    combineReducers(reducers),
+    connectRouter(history)(combineReducers(reducers)),
+    // combineReducers(reducers),
     constants.initialState,
-    applyMiddleware(thunk),
+    compose(applyMiddleware(routerMiddleware(history), Thunk)),
+    // applyMiddleware(Thunk),
 );
 
 render(
-    <BrowserRouter>
-        <Provider store={store}>
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
             <App />
-        </Provider>
-    </BrowserRouter>,
+        </ConnectedRouter>
+    </Provider>,
     document.getElementById('react-app'),
 );
