023fa14472c57c237a2ce4a9623de5ac2d2ad828
Ihar Saroka <Ihar_Saroka@epam.com>
2018-07-09 11:10:20 +0300

    ReduxSagaIntegration: all identity functions in action creators replaced with undefined

--- src/app/actions/articleSelection.js
+++ src/app/actions/articleSelection.js
@@ -1,9 +1,6 @@
 import { createAction } from 'redux-actions';
 import constants from '../constants';
 
-const selectNewArticle = createAction(
-    constants.actions.ARTICLE_SELECTED,
-    article => article,
-);
+const selectNewArticle = createAction(constants.actions.ARTICLE_SELECTED);
 
 export default { selectNewArticle };
--- src/app/actions/loadingNews.js
+++ src/app/actions/loadingNews.js
@@ -9,14 +9,14 @@ const {
     loadNews,
     loadRelatedNews,
 } = createActions({
-    [constants.actions.NEWS_IS_LOADING]: boolValue => boolValue,
     [constants.actions.LOAD_NEWS]: (apiLink, uploadedActionType) => ({
         apiLink,
         uploadedActionType,
     }),
-    [constants.actions.LOAD_RELATED_NEWS]: topNewsId => topNewsId,
-    [constants.actions.TOP_NEWS_DOWNLOADED]: newArticles => newArticles,
-    [constants.actions.RELATED_NEWS_DOWNLOADED]: newArticles => newArticles,
+    [constants.actions.NEWS_IS_LOADING]: undefined,
+    [constants.actions.LOAD_RELATED_NEWS]: undefined,
+    [constants.actions.TOP_NEWS_DOWNLOADED]: undefined,
+    [constants.actions.RELATED_NEWS_DOWNLOADED]: undefined,
 });
 
 export default {
