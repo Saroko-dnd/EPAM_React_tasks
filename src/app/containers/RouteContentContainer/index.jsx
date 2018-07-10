d407f6e5b6995ca7cb0295297da0163a3a3262e2
Ihar Saroka <Ihar_Saroka@epam.com>
2018-07-10 17:43:22 +0300

    GlobalCss: Article image now has max and min width

--- src/app/containers/RouteContentContainer/index.jsx
+++ src/app/containers/RouteContentContainer/index.jsx
@@ -21,7 +21,11 @@ const RouteContentContainer = ({ className }) => (
                     </div>
                 )}
             />
-            <Route exact path="/first" render={() => <h1>Route 2</h1>} />
+            <Route
+                exact
+                path="/first"
+                render={() => <h1>Route 2 рабочий</h1>}
+            />
             <Route exact path="/second" render={() => <h1>Route 3</h1>} />
             <Route exact path="/third" render={() => <h1>Route 4</h1>} />
             <Route
